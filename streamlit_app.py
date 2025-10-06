5702f114d9d29234e582de72eab30c2b6c5b3ebb
joaopaulo433 <46505422+joaopaulo433@users.noreply.github.com>
2025-10-06 13:19:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,12 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-ST.STOP()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#ST.STOP()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 
